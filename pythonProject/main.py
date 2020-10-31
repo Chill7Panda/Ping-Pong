6c85aef48eb8f270e476c10b90b07580be0ed88c
DESKTOP-FKMT2OD\Ivan <svjatun05@gmail.com>
2020-10-31 14:55:31 +0200

    Теніс

---
+++ pythonProject/main.py
@@ -0,0 +1,531 @@
+from tkinter import *
+
+# глобальные переменные
+# настройки окна
+WIDTH = 900
+HEIGHT = 300
+
+# настройки ракеток
+
+# ширина ракетки
+PAD_W = 10
+# высота ракетки
+PAD_H = 100
+
+# настройки мяча
+
+# радиус мяча
+BALL_RADIUS = 30
+
+# устанавливаем окно
+root = Tk()
+root.title("PythonicWay Pong")
+
+# область анимации
+c = Canvas(root, width=WIDTH, height=HEIGHT, background="#003300")
+c.pack()
+
+# элементы игрового поля
+
+# левая линия
+c.create_line(PAD_W, 0, PAD_W, HEIGHT, fill="white")
+# правая линия
+c.create_line(WIDTH - PAD_W, 0, WIDTH - PAD_W, HEIGHT, fill="white")
+# центральная линия
+c.create_line(WIDTH / 2, 0, WIDTH / 2, HEIGHT, fill="white")
+
+# установка игровых объектов
+
+# создаем мяч
+BALL = c.create_oval(WIDTH / 2 - BALL_RADIUS / 2,
+                     HEIGHT / 2 - BALL_RADIUS / 2,
+                     WIDTH / 2 + BALL_RADIUS / 2,
+                     HEIGHT / 2 + BALL_RADIUS / 2, fill="white")
+
+# левая ракетка
+LEFT_PAD = c.create_line(PAD_W / 2, 0, PAD_W / 2, PAD_H, width=PAD_W, fill="yellow")
+
+# правая ракетка
+RIGHT_PAD = c.create_line(WIDTH - PAD_W / 2, 0, WIDTH - PAD_W / 2,
+                          PAD_H, width=PAD_W, fill="yellow")
+
+# запускаем работу окна
+root.mainloop()
+BALL_X_CHANGE = 20
+# по вертикали
+BALL_Y_CHANGE = 0
+
+
+def move_ball():
+    c.move(BALL, BALL_X_CHANGE, BALL_Y_CHANGE)
+
+
+def main():
+    move_ball()
+    # вызываем саму себя каждые 30 миллисекунд
+    root.after(30, main)
+
+
+# запускаем движение
+main()
+# зададим глобальные переменные скорости движения ракеток
+# скорось с которой будут ездить ракетки
+PAD_SPEED = 20
+# скорость левой платформы
+LEFT_PAD_SPEED = 0
+# скорость правой ракетки
+RIGHT_PAD_SPEED = 0
+
+
+# функция движения обеих ракеток
+def move_pads():
+    # для удобства создадим словарь, где ракетке соответствует ее скорость
+    PADS = {LEFT_PAD: LEFT_PAD_SPEED,
+            RIGHT_PAD: RIGHT_PAD_SPEED}
+    # перебираем ракетки
+    for pad in PADS:
+        # двигаем ракетку с заданной скоростью
+        c.move(pad, 0, PADS[pad])
+        # если ракетка вылезает за игровое поле возвращаем ее на место
+        if c.coords(pad)[1] < 0:
+            c.move(pad, 0, -c.coords(pad)[1])
+        elif c.coords(pad)[3] > HEIGHT:
+            c.move(pad, 0, HEIGHT - c.coords(pad)[3])
+
+
+# Вставляем созданную функцию в main
+def main():
+    move_ball()
+    move_pads()
+    root.after(30, main)
+
+
+# Установим фокус на Canvas чтобы он реагировал на нажатия клавиш
+c.focus_set()
+
+
+# Напишем функцию обработки нажатия клавиш
+def movement_handler(event):
+    global LEFT_PAD_SPEED, RIGHT_PAD_SPEED
+    if event.keysym == "w":
+        LEFT_PAD_SPEED = -PAD_SPEED
+    elif event.keysym == "s":
+        LEFT_PAD_SPEED = PAD_SPEED
+    elif event.keysym == "Up":
+        RIGHT_PAD_SPEED = -PAD_SPEED
+    elif event.keysym == "Down":
+        RIGHT_PAD_SPEED = PAD_SPEED
+
+
+# Привяжем к Canvas эту функцию
+c.bind("<KeyPress>", movement_handler)
+
+
+# Создадим функцию реагирования на отпускание клавиши
+def stop_pad(event):
+    global LEFT_PAD_SPEED, RIGHT_PAD_SPEED
+    if event.keysym in "ws":
+        LEFT_PAD_SPEED = 0
+    elif event.keysym in ("Up", "Down"):
+        RIGHT_PAD_SPEED = 0
+
+
+# Привяжем к Canvas эту функцию
+c.bind("<KeyRelease>", stop_pad)
+from tkinter import *
+
+# глобальные переменные
+# настройки окна
+WIDTH = 900
+HEIGHT = 300
+
+# настройки ракеток
+
+# ширина ракетки
+PAD_W = 10
+# высота ракетки
+PAD_H = 100
+
+# настройки мяча
+
+# радиус мяча
+BALL_RADIUS = 30
+
+# устанавливаем окно
+root = Tk()
+root.title("PythonicWay Pong")
+
+# область анимации
+c = Canvas(root, width=WIDTH, height=HEIGHT, background="#003300")
+c.pack()
+
+# элементы игрового поля
+
+# левая линия
+c.create_line(PAD_W, 0, PAD_W, HEIGHT, fill="white")
+# правая линия
+c.create_line(WIDTH - PAD_W, 0, WIDTH - PAD_W, HEIGHT, fill="white")
+# центральная линия
+c.create_line(WIDTH / 2, 0, WIDTH / 2, HEIGHT, fill="white")
+
+# установка игровых объектов
+
+# создаем мяч
+BALL = c.create_oval(WIDTH / 2 - BALL_RADIUS / 2,
+                     HEIGHT / 2 - BALL_RADIUS / 2,
+                     WIDTH / 2 + BALL_RADIUS / 2,
+                     HEIGHT / 2 + BALL_RADIUS / 2, fill="white")
+
+# левая ракетка
+LEFT_PAD = c.create_line(PAD_W / 2, 0, PAD_W / 2, PAD_H, width=PAD_W, fill="yellow")
+
+# правая ракетка
+RIGHT_PAD = c.create_line(WIDTH - PAD_W / 2, 0, WIDTH - PAD_W / 2,
+                          PAD_H, width=PAD_W, fill="yellow")
+
+
+
+
+def move_ball():
+    c.move(BALL, BALL_X_CHANGE, BALL_Y_CHANGE)
+
+
+def main():
+    move_ball()
+    # вызываем саму себя каждые 30 миллисекунд
+    root.after(30, main)
+
+
+# запускаем движение
+main()
+# зададим глобальные переменные скорости движения ракеток
+# скорось с которой будут ездить ракетки
+PAD_SPEED = 20
+# скорость левой платформы
+LEFT_PAD_SPEED = 0
+# скорость правой ракетки
+RIGHT_PAD_SPEED = 0
+
+
+# функция движения обеих ракеток
+def move_pads():
+    # для удобства создадим словарь, где ракетке соответствует ее скорость
+    PADS = {LEFT_PAD: LEFT_PAD_SPEED,
+            RIGHT_PAD: RIGHT_PAD_SPEED}
+    # перебираем ракетки
+    for pad in PADS:
+        # двигаем ракетку с заданной скоростью
+        c.move(pad, 0, PADS[pad])
+        # если ракетка вылезает за игровое поле возвращаем ее на место
+        if c.coords(pad)[1] < 0:
+            c.move(pad, 0, -c.coords(pad)[1])
+        elif c.coords(pad)[3] > HEIGHT:
+            c.move(pad, 0, HEIGHT - c.coords(pad)[3])
+
+
+# Вставляем созданную функцию в main
+def main():
+    move_ball()
+    move_pads()
+    root.after(30, main)
+
+
+# Установим фокус на Canvas чтобы он реагировал на нажатия клавиш
+c.focus_set()
+
+
+# Напишем функцию обработки нажатия клавиш
+def movement_handler(event):
+    global LEFT_PAD_SPEED, RIGHT_PAD_SPEED
+    if event.keysym == "w":
+        LEFT_PAD_SPEED = -PAD_SPEED
+    elif event.keysym == "s":
+        LEFT_PAD_SPEED = PAD_SPEED
+    elif event.keysym == "Up":
+        RIGHT_PAD_SPEED = -PAD_SPEED
+    elif event.keysym == "Down":
+        RIGHT_PAD_SPEED = PAD_SPEED
+
+
+# Привяжем к Canvas эту функцию
+c.bind("<KeyPress>", movement_handler)
+
+
+# Создадим функцию реагирования на отпускание клавиши
+def stop_pad(event):
+    global LEFT_PAD_SPEED, RIGHT_PAD_SPEED
+    if event.keysym in "ws":
+        LEFT_PAD_SPEED = 0
+    elif event.keysym in ("Up", "Down"):
+        RIGHT_PAD_SPEED = 0
+
+
+# Привяжем к Canvas эту функцию
+c.bind("<KeyRelease>", stop_pad)
+from tkinter import *
+
+# глобальные переменные
+# настройки окна
+WIDTH = 900
+HEIGHT = 300
+
+# настройки ракеток
+
+# ширина ракетки
+PAD_W = 10
+# высота ракетки
+PAD_H = 100
+
+# настройки мяча
+
+# радиус мяча
+BALL_RADIUS = 30
+
+# устанавливаем окно
+root = Tk()
+root.title("PythonicWay Pong")
+
+# область анимации
+c = Canvas(root, width=WIDTH, height=HEIGHT, background="#003300")
+c.pack()
+
+c.create_line(PAD_W, 0, PAD_W, HEIGHT, fill="white")
+# правая линия
+c.create_line(WIDTH - PAD_W, 0, WIDTH - PAD_W, HEIGHT, fill="white")
+# центральная линия
+c.create_line(WIDTH / 2, 0, WIDTH / 2, HEIGHT, fill="white")
+
+# установка игровых объектов
+
+# создаем мяч
+BALL = c.create_oval(WIDTH / 2 - BALL_RADIUS / 2,
+                     HEIGHT / 2 - BALL_RADIUS / 2,
+                     WIDTH / 2 + BALL_RADIUS / 2,
+                     HEIGHT / 2 + BALL_RADIUS / 2, fill="white")
+
+# левая ракетка
+LEFT_PAD = c.create_line(PAD_W / 2, 0, PAD_W / 2, PAD_H, width=PAD_W, fill="yellow")
+
+# правая ракетка
+RIGHT_PAD = c.create_line(WIDTH - PAD_W / 2, 0, WIDTH - PAD_W / 2,
+                          PAD_H, width=PAD_W, fill="yellow")
+
+# запускаем работу окна
+root.mainloop()
+BALL_X_CHANGE = 20
+# по вертикали
+BALL_Y_CHANGE = 0
+
+
+def move_ball():
+    c.move(BALL, BALL_X_CHANGE, BALL_Y_CHANGE)
+
+
+def main():
+    move_ball()
+    # вызываем саму себя каждые 30 миллисекунд
+    root.after(30, main)
+
+
+# запускаем движение
+main()
+# зададим глобальные переменные скорости движения ракеток
+# скорось с которой будут ездить ракетки
+PAD_SPEED = 20
+# скорость левой платформы
+LEFT_PAD_SPEED = 0
+# скорость правой ракетки
+RIGHT_PAD_SPEED = 0
+
+
+# функция движения обеих ракеток
+def move_pads():
+    # для удобства создадим словарь, где ракетке соответствует ее скорость
+    PADS = {LEFT_PAD: LEFT_PAD_SPEED,
+            RIGHT_PAD: RIGHT_PAD_SPEED}
+    # перебираем ракетки
+    for pad in PADS:
+        # двигаем ракетку с заданной скоростью
+        c.move(pad, 0, PADS[pad])
+        # если ракетка вылезает за игровое поле возвращаем ее на место
+        if c.coords(pad)[1] < 0:
+            c.move(pad, 0, -c.coords(pad)[1])
+        elif c.coords(pad)[3] > HEIGHT:
+            c.move(pad, 0, HEIGHT - c.coords(pad)[3])
+
+
+# Вставляем созданную функцию в main
+def main():
+    move_ball()
+    move_pads()
+    root.after(30, main)
+
+
+# Установим фокус на Canvas чтобы он реагировал на нажатия клавиш
+c.focus_set()
+
+
+# Напишем функцию обработки нажатия клавиш
+def movement_handler(event):
+    global LEFT_PAD_SPEED, RIGHT_PAD_SPEED
+    if event.keysym == "w":
+        LEFT_PAD_SPEED = -PAD_SPEED
+    elif event.keysym == "s":
+        LEFT_PAD_SPEED = PAD_SPEED
+    elif event.keysym == "Up":
+        RIGHT_PAD_SPEED = -PAD_SPEED
+    elif event.keysym == "Down":
+        RIGHT_PAD_SPEED = PAD_SPEED
+
+
+# Привяжем к Canvas эту функцию
+c.bind("<KeyPress>", movement_handler)
+
+
+# Создадим функцию реагирования на отпускание клавиши
+def stop_pad(event):
+    global LEFT_PAD_SPEED, RIGHT_PAD_SPEED
+    if event.keysym in "ws":
+        LEFT_PAD_SPEED = 0
+    elif event.keysym in ("Up", "Down"):
+        RIGHT_PAD_SPEED = 0
+
+
+# Привяжем к Canvas эту функцию
+c.bind("<KeyRelease>", stop_pad)
+from tkinter import *
+
+# глобальные переменные
+# настройки окна
+WIDTH = 900
+HEIGHT = 300
+
+# настройки ракеток
+
+# ширина ракетки
+PAD_W = 10
+# высота ракетки
+PAD_H = 100
+
+# настройки мяча
+
+# радиус мяча
+BALL_RADIUS = 30
+
+# устанавливаем окно
+root = Tk()
+root.title("PythonicWay Pong")
+
+# область анимации
+c = Canvas(root, width=WIDTH, height=HEIGHT, background="#003300")
+c.pack()
+
+# элементы игрового поля
+
+# левая линия
+c.create_line(PAD_W, 0, PAD_W, HEIGHT, fill="white")
+# правая линия
+c.create_line(WIDTH - PAD_W, 0, WIDTH - PAD_W, HEIGHT, fill="white")
+# центральная линия
+c.create_line(WIDTH / 2, 0, WIDTH / 2, HEIGHT, fill="white")
+
+# установка игровых объектов
+
+# создаем мяч
+BALL = c.create_oval(WIDTH / 2 - BALL_RADIUS / 2,
+                     HEIGHT / 2 - BALL_RADIUS / 2,
+                     WIDTH / 2 + BALL_RADIUS / 2,
+                     HEIGHT / 2 + BALL_RADIUS / 2, fill="white")
+
+# левая ракетка
+LEFT_PAD = c.create_line(PAD_W / 2, 0, PAD_W / 2, PAD_H, width=PAD_W, fill="yellow")
+
+# правая ракетка
+RIGHT_PAD = c.create_line(WIDTH - PAD_W / 2, 0, WIDTH - PAD_W / 2,
+                          PAD_H, width=PAD_W, fill="yellow")
+
+# запускаем работу окна
+root.mainloop()
+BALL_X_CHANGE = 20
+# по вертикали
+BALL_Y_CHANGE = 0
+
+
+def move_ball():
+    c.move(BALL, BALL_X_CHANGE, BALL_Y_CHANGE)
+
+
+def main():
+    move_ball()
+    # вызываем саму себя каждые 30 миллисекунд
+    root.after(30, main)
+
+
+# запускаем движение
+main()
+# зададим глобальные переменные скорости движения ракеток
+# скорось с которой будут ездить ракетки
+PAD_SPEED = 20
+# скорость левой платформы
+LEFT_PAD_SPEED = 0
+# скорость правой ракетки
+RIGHT_PAD_SPEED = 0
+
+
+# функция движения обеих ракеток
+def move_pads():
+    # для удобства создадим словарь, где ракетке соответствует ее скорость
+    PADS = {LEFT_PAD: LEFT_PAD_SPEED,
+            RIGHT_PAD: RIGHT_PAD_SPEED}
+    # перебираем ракетки
+    for pad in PADS:
+        # двигаем ракетку с заданной скоростью
+        c.move(pad, 0, PADS[pad])
+        # если ракетка вылезает за игровое поле возвращаем ее на место
+        if c.coords(pad)[1] < 0:
+            c.move(pad, 0, -c.coords(pad)[1])
+        elif c.coords(pad)[3] > HEIGHT:
+            c.move(pad, 0, HEIGHT - c.coords(pad)[3])
+
+
+# Вставляем созданную функцию в main
+def main():
+    move_ball()
+    move_pads()
+    root.after(30, main)
+
+
+# Установим фокус на Canvas чтобы он реагировал на нажатия клавиш
+c.focus_set()
+
+
+# Напишем функцию обработки нажатия клавиш
+def movement_handler(event):
+    global LEFT_PAD_SPEED, RIGHT_PAD_SPEED
+    if event.keysym == "w":
+        LEFT_PAD_SPEED = -PAD_SPEED
+    elif event.keysym == "s":
+        LEFT_PAD_SPEED = PAD_SPEED
+    elif event.keysym == "Up":
+        RIGHT_PAD_SPEED = -PAD_SPEED
+    elif event.keysym == "Down":
+        RIGHT_PAD_SPEED = PAD_SPEED
+
+
+# Привяжем к Canvas эту функцию
+c.bind("<KeyPress>", movement_handler)
+
+
+# Создадим функцию реагирования на отпускание клавиши
+def stop_pad(event):
+    global LEFT_PAD_SPEED, RIGHT_PAD_SPEED
+    if event.keysym in "ws":
+        LEFT_PAD_SPEED = 0
+    elif event.keysym in ("Up", "Down"):
+        RIGHT_PAD_SPEED = 0
+
+
+# Привяжем к Canvas эту функцию
+c.bind("<KeyRelease>", stop_pad)
+
+і
